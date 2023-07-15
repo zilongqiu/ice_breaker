fbee71b2f88d16f31566bb847f14d5714937282a
Zi-long QIU <zilong.qiu@gmail.com>
2023-07-15 16:45:26 +0800

    [lesson5] feat: add output parser and print to json

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,14 +1,16 @@
+from typing import Tuple
+import json
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
 
-name = "Elon Musk"
-if __name__ == "__main__":
-    print("Hello Langchain!")
 
+def ice_breaker(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -17,15 +19,29 @@ if __name__ == "__main__":
          1. a short summary
          2. two interesting facts about them
          3. A topic that may interest them
-         4. 2 creative Ice breakers to open a conversation with them 
+         4. 2 creative Ice breakers to open a conversation with them
+         \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["linkedin_information"], template=summary_template
+        input_variables=["linkedin_information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data))
+    result = chain.run(linkedin_information=linkedin_data)
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+    print("Hello Langchain!")
+    result = ice_breaker(name="Harrison Chase")
+    json_object = json.loads(result.json())
+    json_formatted_str = json.dumps(json_object, indent=2)
+    print(json_formatted_str)
