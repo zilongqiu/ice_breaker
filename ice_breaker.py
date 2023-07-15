df8d2eeb25e52b67253ad1ca0a274f8966680876
Zi-long QIU <zilong.qiu@gmail.com>
2023-07-15 15:32:12 +0800

    [lesson3] feat: automate linkedin profile url lookup using an agent and use it to generate facts & message ideas

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,28 +2,30 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
-
+name = "Elon Musk"
 if __name__ == "__main__":
     print("Hello Langchain!")
 
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+
     summary_template = """
-        given the information {information} about a person from I want to you to create:
-        1. a short summary
-        2. two interesting facts about them
+        given the Linkedin information {linkedin_information} about a person from I want to you to create:
+         1. a short summary
+         2. two interesting facts about them
+         3. A topic that may interest them
+         4. 2 creative Ice breakers to open a conversation with them 
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["linkedin_information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
-    )
-
-    print(chain.run(information=linkedin_data))
+    print(chain.run(linkedin_information=linkedin_data))
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -8,16 +8,12 @@ def scrape_linkedin_profile(linkedin_profile_url: str):
 
     ### Example with Linkedin ###
     ### PROXYCURL_API_KEY env config from https://nubela.co/proxycurl/dashboard/proxycurl-api/api-key/
-    # api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    # header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
 
-    # response = requests.get(
-    #     api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
-    # )
-
-    ### Example from gist ###
-    api_endpoint = "https://gist.githubusercontent.com/zilongqiu/da6f8e1837360c3160843ce76e6b52f2/raw/5a1402cac1220accf800c8ef79e184f13bdab950/gistfile1.json"
-    response = requests.get(api_endpoint)
+    response = requests.get(
+        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+    )
 
     data = response.json()
     data = {
